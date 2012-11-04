d4dd1c3255622a7be01b4d3d62442d44cd94896b
Trevor R <tarice.dev@gmail.com>
2012-11-03 20:34:41 -0500

    Added webserver files.

---
+++ src/headcrab_webserver.c
@@ -0,0 +1,37 @@
+#include "mongoose.h"
+
+struct mg_context * ctx;
+
+static void * ws_message_handler(enum mg_event event,
+						  		 struct mg_connection *conn)
+{
+	if (event == MG_WEBSOCKET_CONNECT) {
+
+	} else if (event == MG_WEBSOCKET_MESSAGE) {
+
+	} else if (event == MG_WEBSOCKET_DUNNO) {
+
+	}
+
+	return NULL;
+}
+
+int websocket_initialize(char * _assetDir)
+{
+	// Set options.
+	const char * options[] = {
+		"listening_ports", "8080",
+		"document_root", _assetDir,
+		NULL
+	};
+	// Start web server.
+	ctx = mg_start(&ws_message_handler, NULL, options);
+	if (!ctx)
+		return -1;
+	return 0;
+}
+
+void websocket_shutdown()
+{
+	mg_stop(ctx);
+}
---
+++ src/headcrab_webserver.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef _HEADCRAB_WEBSERVER_H
+#define _HEADCRAB_WEBSERVER_H
+
+int websocket_initialize(char * _assetDir);
+
+void websocket_shutdown();
+
+#endif//_HEADCRAB_WEBSERVER_H
