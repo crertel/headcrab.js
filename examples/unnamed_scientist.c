9cd4d4d8ffb20ebe019a7ff58ad99968bc6ed6cf
Trevor R <tarice.dev@gmail.com>
2012-11-04 00:52:53 -0500

    Updated logging.

--- examples/unnamed_scientist.c
+++ examples/unnamed_scientist.c
@@ -1,8 +1,24 @@
 #include "headcrab.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+int actually_fprintf(const char * stuff, ...)
+{
+	va_list arg_list;
+	va_start(arg_list, stuff);
+	vfprintf(stderr, stuff, arg_list);
+	va_end(arg_list);
+	return 0;
+}
 
 int main(int argc, char ** argv)
 {
 	headcrab_init("./");
-
+	printf("FUCK YOU\n");
+	LOG_MSG("We should not be able to do this.\n");
+	headcrab_set_logging_callback(actually_fprintf);
+	printf("FUCK YOU AGAIN!\n");
+	LOG_MSG("We should not be able to do this.\n");
+	getchar();
 	return 0;
 }
