707adc3dc578081a93f23910e515575fb7e9aadf
Trevor R <tarice.dev@gmail.com>
2012-11-03 20:33:15 -0500

    Implemented all API functions.

--- src/headcrab.c
+++ src/headcrab.c
@@ -1,20 +1,70 @@
 #include "headcrab.h"
-#include "mongoose.h"
 #include "headcrab_dispatch.h"
 
-struct mg_context * ctx;
+HC_ObjectNode * dispatch_table;
 
 HEADCRAB_ERROR headcrab_init(const char* _assetDir)
 {
-	const char * options[] = {
-		"listening_ports", "8080",
-		"document_root", _assetDir,
-		NULL
-	};
-	ctx = mg_start(&callback, NULL, options);
+	// Default logging goes to stdout.
+	log_call = printf;
+	// Initialize the dispatch table.
+	dispatch_table = malloc(sizeof(*new_node));
+	dispatch_table = { "", NULL, NULL, NULL };
+	// Start Websocket Server.
+	LOG_MSG("Connecting to server.");
+	if ( websocket_initialize(_assetDir) ) {
+		LOG_MSG("Connected to server.");
+	} else {
+		LOG_ERROR("Failed to connect to server.");
+		headcrab_shutdown();
+	}
 }
 
 void headcrab_shutdown()
 {
-	mg_stop(ctx);
+	LOG_MSG("Disconnecting from server.");
+	websocket_shutdown();
+	return;
+}
+
+void headcrab_set_logging_callback( HC_LogFunction _callback)
+{
+	log_call = _callback;
+}
+
+void headcrab_bind_object(  void* _target,
+                            const char* _name,
+                            const char* _verb,
+                            HC_PreOpFucntion _preOp,
+                            const void* _preOpArgs,
+                            HC_MutatorFunction _op,
+                            HC_PostOpFucntion _postOp,
+                            const void* _postOpArgs
+                        )
+{
+	// Copy the strings.
+	size_t name_len = strnlen(_name, MAXSTRLEN);
+	char * name = malloc(name_len * sizeof(*_name));
+	strncpy(name, _name, MAXSTRLEN);
+	size_t verb_len = strnlen(_verb, MAXSTRLEN);
+	char * verb = malloc(verb_len * sizeof(*_verb));
+	strncpy(verb, _verb, MAXSTRLEN);
+	// Add to dispatch table.
+	dispatch_table_add(dispatch_table, _target, name,
+						verb, _preOp, _preOpArgs,
+						_op, _postOp, _postOpArgs);
+	return;
+}
+
+void headcrab_clear_all_object_bindings( const char* _name )
+{
+	// Cannot delete the global namespace node.
+	if (*name == '\0')
+		return;
+	dispatch_table_remove(dispatch_table, _name);
+}
+
+void headcrab_rebind_object( const char* _name, void* _object)
+{
+	dispatch_table_rebind(dispatch_table, _name, _object);
 }
