0ea97010345766c7b8e77c5f873f43dc5c647a10
Trevor R <tarice.dev@gmail.com>
2012-11-03 22:27:18 -0500

    Minor modifications.

--- src/headcrab_dispatch.h
+++ src/headcrab_dispatch.h
@@ -1,15 +1,13 @@
-typedef struct
-{
-	char * name;
-	ObjectNode * next;
-	HC_Handler * handler;
-	void * object;
-} HC_ObjectNode;
+#pragma once
+#ifndef HEADER_DISPATCH_H
+#define HEADER_DISPATCH_H
 
-typedef struct
+#include "headcrab.h"
+
+typedef struct HC_Handler
 {
 	char * name;
-	HC_Hlandler * next;
+	struct HC_Handler * next;
 	HC_PreOpFunction * pre;
 	HC_PostOpFunction * post;
 	void * preArgs;
@@ -17,17 +15,29 @@ typedef struct
 	HC_MutatorFunction * op;
 } HC_Handler;
 
+typedef struct HC_ObjectNode
+{
+	char * name;
+	struct ObjectNode * next;
+	struct HC_Handler * handler;
+	void * object;
+} HC_ObjectNode;
+
 void dispatch_table_init();
 
+void free_node(HC_ObjectNode* _node);
+
 void dispatch_table_remove(const char* _name);
+
+void dispatch_table_rebind(const char* _name, void* _target);
 
 HEADCRAB_ERROR dispatch_table_add(  void* _target,
 			                        const char* _name,
 			                        const char* _verb,
-			                        HC_PreOpFucntion _preOp,
+			                        HC_PreOpFunction _preOp,
 			                        const void* _preOpArgs,
 			                        HC_MutatorFunction _op,
-			                        HC_PostOpFucntion _postOp,
+			                        HC_PostOpFunction _postOp,
 			                        const void* _postOpArgs
 									);
 
@@ -38,9 +48,11 @@ HEADCRAB_ERROR add_or_find_node(HC_ObjectNode** out,
 
 void node_add_handler(	HC_ObjectNode* node,
                         const char* _verb,
-                        HC_PreOpFucntion _preOp,
+                        HC_PreOpFunction _preOp,
                         const void* _preOpArgs,
                         HC_MutatorFunction _op,
-                        HC_PostOpFucntion _postOp,
+                        HC_PostOpFunction _postOp,
                         const void* _postOpArgs
                       );
+
+#endif//HEADER_DISPATCH_H
