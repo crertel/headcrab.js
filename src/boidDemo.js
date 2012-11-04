e48754edecaa33f54a1b1e4d17a2eda0d2093df0
Chris Ertel <cre1@762studios.com>
2012-11-04 15:44:10 -0600

    Added headcrab.js.

---
+++ src/boidDemo.js
@@ -0,0 +1,13 @@
+var boidDemo = (function () {
+    var init = function () {
+
+    };
+
+    var reset = function () {
+
+    };
+
+    return {    init: init,
+                reset: reset,
+            };
+})();
