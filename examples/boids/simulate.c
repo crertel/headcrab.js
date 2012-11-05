343f982071962cf043f8d20f20a25cdad61983e2
jmorgensen <morgensen@rice.edu>
2012-11-05 04:04:43 +0100

    Working on cleaning up the directory structure

--- examples/boids/simulate.c
+++ examples/boids/simulate.c
@@ -24,92 +24,118 @@ double angle(Position a, double dir, Position b)
 	return atan((a.y - b.y) / (a.x - b.x)) - dir;
 }
 
-void calculate_view(Boid * _boid, BoidView ** view_list)
+void fill_view_list(Boid * _boid, BoidView * view_list)
 {
 	int j = 0;
 	double dist, ang;
+	Boid *other;
 	for (int i = 0; i < current_boid_count; ++i) {
 		if (i == _boid->boidID)
 			continue;
-		printf("Calc view for %d %d", i, j);
-		dist = distance(_boid->pos, boid_list[i]->pos);
-		printf("Distance! %f\n", dist);
-		ang = angle(_boid->pos, _boid->vel.dir, boid_list[i]->pos);
-		printf("Angle! %f\n", ang);
+		other = &(boid_list[i]);
+		dist = distance(_boid->pos, other->pos);
+		ang = angle(_boid->pos, _boid->vel.dir, other->pos);
 		if (dist < BOID_VIEW_DIST && fabs(ang) < BOID_VIEW_ARC / 2) {
-			(*view_list)[j].boid = boid_list[i];
-			(*view_list)[j].dist_to_boid = dist;
-			(*view_list)[j].angle_to_boid = ang;
+			view_list[j] = {other, dist, ang};
 			j++;
 		}
 	}
-	printf("Boid is here: %p.\n",  (*view_list)[0].boid);
+
 	printf("I found %d matches.\n", j);
 	if (j < current_boid_count)
-		(*view_list)[j].boid = NULL;
-	printf("End of calc view! yay. \n");
+		view_list[j] = {NULL, 0.0, 0.0};
 }
 
-// double calculate_turning(Boid * _boid)
-// {
-
-// }
-
-// double calculate_repulsion(Boid * _boid)
-// {
-
-// }
-
-void calculate_attraction(Boid * _boid, BoidView ** _view_list)
+Accel attraction(Boid * _boid, BoidView * _other)
 {
-	int min_boid_id = 0;
-	double min_dist = 0.0;
-	double d_accel = 0.0;
-	printf("Find closest boid.\n");
-	for (int i = 0; i < current_boid_count && (*_view_list)[i].boid != NULL; ++i) {
-		printf("Find %d\n", i);
-		if ((*_view_list)[i].dist_to_boid > min_dist) {
-			min_boid_id = i;
-			min_dist = (*_view_list)[i].dist_to_boid;
-		}
-	}
-	if ((*_view_list)[0].boid == NULL)
-		return;
-	_boid->vel.dir += 0.1 * (*_view_list)[min_boid_id].angle_to_boid;
-	if (_boid->vel.dir > 2.0 * PI_CONSTANT)
-		_boid->vel.dir -= 2.0 * PI_CONSTANT;
-	else if (_boid->vel.dir < 0)
-		_boid->vel.dir += 2.0 * PI_CONSTANT;
+	Accel accel;
+
+	accel.dir = _other.angle_to_boid;
+	accel.accel = _other.dist_to_boid;
 }
 
-// double calculate_acceleration(Boid * _boid)
-// {
+Accel total_influence(Boid * _boid)
+{
+	int i;
+	BoidView view_list[20];
+	Accel attraction;
+	Accel update;
 
-// }
+	fill_view_list(_boid, view_list);
+
+	for(i = 0; view_list[i].boid != NULL; i++)
+	{
+		Accel attraction = attraction(_boid, &view_list[i]);
+	}
+
+	update = attraction;
+
+	return update;
+}
+
+// dt = 1 does not change the accel
+void update(Boid *_boid, Accel _influence, double _dt=1)
+{
+	_influence.accel /= _dt;
+
+	// Should we set or influence accel?
+
+	// And then update velocity
+
+	// And then update position
+
+	/*
+	dx = boid_list[i]->vel.speed / 
+ 	( sqrt(1 + tan(boid_list[i]->vel.dir) *
+ 			   tan(boid_list[i]->vel.dir))
+	);
+	if (boid_list[i]->pos.x > MAP_WIDTH)
+		boid_list[i]->pos.x -= MAP_WIDTH;
+	if (boid_list[i]->pos.x < 0)
+		boid_list[i]->pos.x += MAP_WIDTH;
+	boid_list[i]->pos.y += dx * tan(boid_list[i]->vel.dir) * _time_step;
+	if (boid_list[i]->pos.y > MAP_HEIGHT)
+		boid_list[i]->pos.y -= MAP_HEIGHT;
+	if (boid_list[i]->pos.y < 0)
+		boid_list[i]->pos.y += MAP_HEIGHT;*/
+}
 
 int update(double _time_step)
 {
-	BoidView view_list[20];
-	double dx;
-	for (int i = 0; i < current_boid_count; ++i) {
-		calculate_view(boid_list[i], &view_list);
-		calculate_attraction(boid_list[i], &view_list);
-		dx = boid_list[i]->vel.speed / 
-		 	( sqrt(1 + tan(boid_list[i]->vel.dir) *
-		 			   tan(boid_list[i]->vel.dir))
-			);
-		boid_list[i]->pos.x += dx * _time_step;
-		if (boid_list[i]->pos.x > MAP_WIDTH)
-			boid_list[i]->pos.x -= MAP_WIDTH;
-		if (boid_list[i]->pos.x < 0)
-			boid_list[i]->pos.x += MAP_WIDTH;
-		boid_list[i]->pos.y += dx * tan(boid_list[i]->vel.dir) * _time_step;
-		if (boid_list[i]->pos.y > MAP_HEIGHT)
-			boid_list[i]->pos.y -= MAP_HEIGHT;
-		if (boid_list[i]->pos.y < 0)
-			boid_list[i]->pos.y += MAP_HEIGHT;
+	int i;
+	Accel influence[20];
+	for (i = 0; i < current_boid_count; i++)
+	{
+		influence[i] = total_influence(&boid_list[i]);
 	}
+
+	for (i = 0; i < current_boid_count; i++)
+	{
+		update(&boid_list[i], influence[i], _time_step);
+	}
+
 	return 0;
+}
+
+json_t *json_boid_data(Boid *_boid)
+{
+	json_t * boid_data = json_object();
+	json_object_set_new(boid_data, "x", json_real(_boid->pos.x));
+	json_object_set_new(boid_data, "y", json_real(_boid->pos.y));
+	json_object_set_new(boid_data, "theta", json_real(_boid->vel.dir));
+	return boid_data;
+}
+
+json_t *json_boid_array()
+{
+	json_t *boids = json_array();
+
+	for(int i = 0; i < current_boid_count; i++)
+	{
+		json_array_append_new(boids, json_boid_data(&boid_list[i]));
+	}
+
+	return boids;
 }
 
 void send_data_to_browser(void* _target, const json_t* _args)
@@ -117,16 +143,8 @@ void send_data_to_browser(void* _target, const json_t* _args)
 	json_t * message = json_object();
 	json_object_set_new(message, "type", json_string("response"));
 	json_t * data = json_object();
-	json_t * boids = json_array();
 
-	for(int i = 0; i < current_boid_count; ++i) {
-		json_t * boid_data = json_object();
-		json_object_set_new(boid_data, "x", json_real(boid_list[i]->pos.x));
-		json_object_set_new(boid_data, "y", json_real(boid_list[i]->pos.y));
-		json_object_set_new(boid_data, "theta", json_real(boid_list[i]->vel.dir));
-		json_array_append_new(boids, boid_data);
-	}
-	json_object_set_new(data, "boids", boids);
+	json_object_set_new(data, "boids", json_boid_array());
 	json_object_set_new(message, "data", data);
 
 	json_t * seq_id = json_object_get(_args, "seqID");
@@ -136,6 +154,11 @@ void send_data_to_browser(void* _target, const json_t* _args)
 }
 
 static int shutdownServer = 0;
+static int running = 1;
+
+HEADCRAB_ERROR pause(void* _args) { running = 0; }
+
+HEADCRAB_ERROR unpause(void* _args) { running = 1; }
 
 void quit_simulation(void* _target, const json_t* _args)
 {
@@ -153,9 +176,9 @@ int main(int argc, char ** argv)
 	printf("Got to #1\n");
 	headcrab_init("");
 	headcrab_bind_object(boid_list, "flock", "getall",
-						 NULL, NULL,
+						 pause, NULL,
 						 send_data_to_browser,
-						 NULL, NULL);
+						 unpause, NULL);
 	// headcrab_bind_object(NULL, "", "shutdown",
 	// 					 NULL, NULL,
 	// 					 quit_simulation,
@@ -163,7 +186,10 @@ int main(int argc, char ** argv)
 
 	while(!shutdownServer)
 	{
-		update(0.001);
+		if (running)
+		{
+			update(0.001);			
+		}
 		headcrab_handle_commands();
 	}
 
